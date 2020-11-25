41d913fdd77171b1f261dccc04e55a5fee4930e8
TJ-Dewey <63527902+TJ-Dewey@users.noreply.github.com>
2020-11-25 11:25:12 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,10 +1,12 @@
+import os
+
 class Server:
     """ Server class for representing and manipulating servers. """
 
     def __init__(self, server_ip):
-        # TODO -
-        self.server_ip = server_ip
+        self.ip = server_ip
 
     def ping(self):
-        # TODO - Use os module to ping the server
-        return
+        address = self.ip
+        response = os.system("ping " + address)
+        return response
